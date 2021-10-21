95af5422110fe8fc4a3e22d75426440780cef70a
rachelfras <82391537+rachelfras@users.noreply.github.com>
2021-10-21 22:32:09 +0100

    Copied CSS from old app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,73 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import Current from "./Current";
+
 
 export default function App() {
   return (
     <div className="App">
-      <div className="container">
-        <section className="current-weather">
+      <div className="widget">
+        <header>
+          <form>
+        <div className="input-group">
+          <input type="text"
+              className="form-control form-bar enter"
+              placeholder="Enter City"
+              //onChange={handleValue}
+          />
+          <div className="input-group-append" id="button-addon4">
+            <input
+              type="submit"
+              className="btn btn-outline-secondary search form-bar"
+              value="Search"
+            />
+            <button
+              className="btn btn-outline-secondary current form-bar"
+              type="button"
+              //onClick={searchGeoLocation}
+            >
+              Current
+            </button>
+          </div>
+        </div>
+      </form>
+        </header>
           <Current />
-        </section>
+          <div className="forecast">
+          <div id="forecast-box" className="container">
+            <div className="row">
+            </div>
+          </div>
+        </div>
+        </div>
+          <footer className="credit">
+            <div>
+            <a
+          href="https://github.com/rachelfras/weather-app-react"
+          class="git-link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>
+        <p> by Rachel Fraser</p>
+        <div class="icon-credit">
+          Icons made by 
+          <a
+            href="https://www.flaticon.com/authors/iconixar"
+            title="iconixar"
+            target="_blank"
+            rel="noreferrer"
+            > iconixar </a
+          >
+           from 
+          <a href="https://www.flaticon.com/" title="Flaticon" target="_blank" rel="noreferrer"
+            > www.flaticon.com</a
+          >
+        </div>
+        </div>
+          </footer>
       </div>
-    </div>
   );
 }
